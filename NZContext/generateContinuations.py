30e2f0897f0bbcb283bb176ef37d468ae7c51cc8
Nathan <nbel806@aucklanduni.ac.nz>
2024-06-14 16:44:05 +1200

    feat: adds other models

--- NZContext/generateContinuations.py
+++ NZContext/generateContinuations.py
@@ -1,10 +1,20 @@
-from transformers import pipeline
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+import torch
 def generate_continuations(prompts,masks,model_type, num_continuations):
-    model = None
+    generator = None
     if model_type == "gpt2":
-        model = pipeline("text-generation", model="gpt2")
+        generator = pipeline("text-generation", model="gpt2")
+    elif model_type == 'bert':
+        generator = pipeline("text-generation", model="distilbert/distilgpt2")
+    elif model_type == 'phi':
+        generator = pipeline("text-generation", model="microsoft/phi-2", trust_remote_code=True)
+    elif model_type == 'llama':
+        generator = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto") #~2 hours for 2gen
+    elif model_type == 'gemma':
+        generator = pipeline("text-generation", model="google/gemma-7b", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto") #Long run plus repeating output
 
     unmasked_prompts = []
+    
 
     for i in range(len(prompts)):
         for j in range(len(masks)):
@@ -15,7 +25,7 @@ def generate_continuations(prompts,masks,model_type, num_continuations):
     for prompt in unmasked_prompts:
         continuations_group = []
         for i in range(num_continuations):
-            generation = model(prompt,   pad_token_id=50256, truncation=True, num_beams=1)
+            generation = generator(prompt, pad_token_id=50256, max_length=30, truncation=True)
             continuation = generation[0]['generated_text'].split('\n\n')[0]
             # continuation = generation[0]['generated_text'].split('.')[0] + '.'
             continuations_group.append(continuation)
