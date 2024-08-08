6aaff3cc7adabf7ea896ff8bccedc243d16cd282
Nathan <nbel806@aucklanduni.ac.nz>
2024-08-09 00:28:38 +1200

    feat: init pages and navbar

--- app/llm-bias-comparison-app/src/app/about/page.tsx
+++ app/llm-bias-comparison-app/src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <h1>Home</h1>;
+export default function About() {
+  return <h1>About</h1>;
 }
