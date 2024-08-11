e57940b1785544fef14db03261ef7f7483d308e4
Nathan <nbel806@aucklanduni.ac.nz>
2024-08-11 13:49:16 +1200

    fix:works on both os with python3

--- app/backend/src/python/execute-python.js
+++ app/backend/src/python/execute-python.js
@@ -1,20 +1,22 @@
 import { exec } from "child_process";
 import util from "util";
 import { resolve } from "path";
-import { promises as fs } from "fs";
 
 const execAsync = util.promisify(exec);
 
 export async function runPythonTestScript(executableName) {
   try {
-    // Get current working directory
+    // Get current working directory and resolve the path to the Python script
     const executablePath = resolve(
       process.cwd(),
       `./src/python/${executableName}`
     );
 
-    await fs.chmod(executablePath, 0o755);
-    const { stdout, stderr } = await execAsync(executablePath);
+    // Construct the full command to run the script with the Python interpreter
+    const command = `${"python3"} ${executablePath}`;
+
+    // Execute the command
+    const { stdout, stderr } = await execAsync(command);
 
     if (stderr) {
       throw new Error(stderr);
@@ -23,41 +25,7 @@ export async function runPythonTestScript(executableName) {
     console.log(`Executable output: ${stdout}`);
     return stdout;
   } catch (error) {
-    console.error(`Error executing executable: ${error.message}`);
-    throw new Error(`Error executing executable: ${error.message}`);
+    console.error(`Error executing script: ${error.message}`);
+    throw new Error(`Error executing script: ${error.message}`);
   }
 }
-
-// import { exec } from "child_process";
-// import { promises as fs } from "fs";
-// import { resolve } from "path";
-
-// import util from "util";
-
-// const execAsync = util.promisify(exec);
-
-// export async function runPythonTestScript(executableName) {
-//   try {
-//     // Get the current working directory
-//     const executablePath = resolve(
-//       process.cwd(),
-//       `./src/python/${executableName}`
-//     );
-
-//     // Set the file to be executable (chmod +x)
-//     await fs.chmod(executablePath, 0o755); // 0o755 gives the owner execute permissions
-
-//     // Execute the script
-//     const { stdout, stderr } = await execAsync(executablePath);
-
-//     if (stderr) {
-//       throw new Error(stderr);
-//     }
-
-//     console.log(`Executable output: ${stdout}`);
-//     return stdout;
-//   } catch (error) {
-//     console.error(`Error executing executable: ${error.message}`);
-//     throw new Error(`Error executing executable: ${error.message}`);
-//   }
-// }
--- app/backend/src/python/pythonTest.py
+++ app/backend/src/python/pythonTest.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python3
 import time
 time.sleep(5)
 print("Hello World!")
