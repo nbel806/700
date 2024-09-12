a1eb19daf6fe44c60d631e48681034ed147a5473
tmur647 <tmur647@aucklanduni.ac.nz>
2024-09-13 10:58:09 +1200

    download data and analyse data buttons not greyed out if data

--- app/backend/src/routes/api/generate.js
+++ app/backend/src/routes/api/generate.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { runPythonScript } from "../../../src/python/execute-app-dot-py.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import fs from "fs/promises";
 
 const generateRouter = Router();
 
@@ -48,6 +49,28 @@ generateRouter.get("/download", async (req, res) => {
         res.status(500).json({ error: "Error downloading the file" });
       }
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// GET /api/generate/check
+generateRouter.get("/check", async (req, res) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  try {
+    const jsonFilePath = join(
+      __dirname,
+      "../../../src/data/llm_generated.json"
+    );
+
+    // Read and Check if the JSON content is empty
+    const fileContent = await fs.readFile(jsonFilePath, "utf8");
+    if (fileContent.trim().length === 0) {
+      return res.json({ isEmpty: true });
+    } else {
+      return res.json({ isEmpty: false });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
